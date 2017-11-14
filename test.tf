b13c151928b5f093f237b8974abfa3ac6f397e15
Ruslan Kazakov <ruslan.kazakov@gmail.com>
2017-11-14 23:54:39 +1100

    Create Bible API EC2 instance

--- test.tf
+++ test.tf
@@ -1,4 +1,11 @@
-resource "aws_instance" "example" {
-  ami           = "ami-2757f631"
+provider "aws" {
+  region = "ap-southeast-2"
+}
+
+resource "aws_instance" "bibleapi-rest" {
+  ami           = "ami-41c12e23" # http://cloud-images.ubuntu.com/locator/ec2/
   instance_type = "t2.micro"
+  tags {
+    Name = "Bible API RESTful Web Service"
+  }
 }
