8307119fb2505b27ca98ca81e054555a9cbf4f63
Ruslan Kazakov <ruslan.kazakov@gmail.com>
2017-11-24 14:55:24 +1100

    Add EC2 instance ingress access

--- ec2.tf
+++ ec2.tf
@@ -7,6 +7,20 @@ resource "aws_security_group" "bibleapi_security_group" {
   description = "Bible API security group"
   vpc_id = "${var.vpc_id}"
 
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 3000
+    to_port = 3000
+    protocol = "tcp"
+    cidr_blocks = [ "0.0.0.0/0" ]
+  }
+
   ingress {
     from_port = 22
     to_port = 22
@@ -14,11 +28,12 @@ resource "aws_security_group" "bibleapi_security_group" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  egress {
-      from_port = 0
-      to_port = 0
-      protocol = "-1"
-      cidr_blocks = ["0.0.0.0/0"]
+  # Ingress access within this security group
+  ingress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    self = true
   }
 }
 
